f978b81c29fc9d7a5305d7f0096d97f7c789b077
Abhishek Singh <jonu.1504@gmail.com>
2019-02-05 20:38:01 +0530

    [express]: separation of concern

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-const app = require('express')();
-const passport = require('passport');
 
-// Bootstrap schemas, models
-require("./src/bootstrap");
+let app = require("./src/express");
 
-// App configuration
-require("./src/serverConfig")(app, passport);
-
-// Add route
-require("./src/config/route")(app, passport);
-
+const http = require('http');
+app = http.createServer(app);
 //listen on PORT
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('process listening ON', process.env.PORT || 3000);
 });
-
 
 module.exports = server;
